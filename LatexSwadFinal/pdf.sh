f01e584d20a3f73401fb2e6ae1b3a4b90dd0dfb6
SarcamoCronico <magoencaja@gmail.com>
2026-06-11 12:11:57 +0200

    ajustes de latex

--- LatexSwadFinal/pdf.sh
+++ LatexSwadFinal/pdf.sh
@@ -23,14 +23,16 @@ while getopts "hrsm:" opt; do
 done
 
 if [[ "$borrar_logs" -eq "1" ]]; then
-    pdflatex  -synctex=1 -interaction=$interactivo -file-line-error  "./$archivo.tex"
+    xelatex  -synctex=1 -interaction=$interactivo -file-line-error  "./$archivo.tex"
     biber $archivo
-    pdflatex  -synctex=1 -interaction=$interactivo -file-line-error  "./$archivo.tex"
-    pdflatex  -synctex=1 -interaction=$interactivo -file-line-error  "./$archivo.tex"
+    xelatex  -synctex=1 -interaction=$interactivo -file-line-error  "./$archivo.tex"
+    xelatex  -synctex=1 -interaction=$interactivo -file-line-error  "./$archivo.tex"
+
     ls | grep -P "$archivo\.(?!(pdf|tex)$)" | xargs rm #-v
 else
-    pdflatex  -synctex=1 -interaction=$interactivo -file-line-error -recorder  "./$archivo.tex"
+    xelatex  -synctex=1 -interaction=$interactivo -file-line-error -recorder  "./$archivo.tex"
     biber $archivo
-    pdflatex  -synctex=1 -interaction=$interactivo -file-line-error  "./$archivo.tex"
-    pdflatex  -synctex=1 -interaction=$interactivo -file-line-error  "./$archivo.tex"
+    xelatex  -synctex=1 -interaction=$interactivo -file-line-error  "./$archivo.tex"
+    xelatex  -synctex=1 -interaction=$interactivo -file-line-error  "./$archivo.tex"
+
 fi
